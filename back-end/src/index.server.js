edbd8d32662b52f519d8fb2c293af5c39ae0c427
Mazidul Islam <mazidulislammym@gmail.com>
2022-06-05 23:40:47 +0600

    second commit

---
+++ back-end/src/index.server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+
+//environment variable or you can say constants
+env.config();
+
+// mongodb connection string 
+// mongodb+srv://<username>:<password>@cluster0.nu4vl.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.nu4vl.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+).then(()=>{
+    console.log("Database connected")
+});
+// mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.nu4vl.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+// {userNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+//     console.log("Database connected")
+// });
+
+
+// app.use(express.json());
+app.use(bodyParser.json());
+
+
+// Api 
+app.get('/', ( req,res, next)=>{
+res.status(200).json({
+    message: "Hello from Server"
+});
+});
+
+
+app.post('/data', ( req,res, next)=>{
+res.status(200).json({
+    message: req.body
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
